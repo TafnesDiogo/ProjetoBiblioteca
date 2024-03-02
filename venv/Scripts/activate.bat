a678d05fc5bd13da2993e03028f2600ec3524ebb
Tafnes Diogo <tafnes.diogo@gmail.com>
2024-03-02 16:39:06 -0300

    Alterações Atividade 1

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\manto\AULASUNISAGRADO\ProjetoBiblioteca\venv
+set VIRTUAL_ENV=C:\Users\Ryzen\Documents\1 Projetos\Tecnologias e Programação Integrada\ProjetoBiblioteca\ProjetoBiblioteca\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
